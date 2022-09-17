1157846b68f86861fb7e2b42df4243443936f71d
arsalanusmanstar <arsalanusmanstar@gmail.com>
2022-09-17 16:56:08 +0500

    reverse_words

---
+++ reverse_words/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+import './App.css';
+
+function App() {
+  const [name, setName] = useState();
+
+  const ReversWords = () => {
+    if(name){
+      let array = name.split(' ')
+      let wordReverse = array.map((word)=>
+          word.split('').reverse().join('')
+      )    
+      let joinWords = wordReverse.join(' ')
+      return joinWords;
+    }else{
+      return 'Write sentences.'
+    }
+  }
+  
+  return (
+    <div className="App">
+      <form>
+        <input type="text" placeholder="Name" onChange={(x)=>setName(x.target.value)} />
+        <br />
+        <ReversWords />
+      </form>
+    </div>
+  );
+}
+
+export default App;
